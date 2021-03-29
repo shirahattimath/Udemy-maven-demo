2d2b49017ad7b8479ad263982fc206fcc850525d
shirahattimath <shirahattimath@gmail.com>
2021-03-29 11:37:33 -0400

    added GitTest.java

---
+++ src/main/java/com/QA_Automation/maven_demo/GitTest.java
@@ -0,0 +1,10 @@
+package com.QA_Automation.maven_demo;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git push test");
+	}
+
+}
